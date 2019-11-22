9b42c77d4c18f5a9dedb265702d350ef590e968d
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-11-22 07:13:25 +0000

    build based on f6a733d

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#ShapeFromShading.jl-1","page":"Home","title":"ShapeFromShading.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"Documentation for ShapeFromShading.jl Work in progress mainly for testing at this point.","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Frankot(x)","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Path(x)","category":"page"}]
+[{"location":"#ShapeFromShading.jl-1","page":"Home","title":"ShapeFromShading.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"Documentation for ShapeFromShading.jl Work in progress mainly for testing at this point.","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Frankot()","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Path()","category":"page"}]
 }
